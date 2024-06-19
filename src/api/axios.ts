445b8ee3fc3877137d47d261c881408b9aae5cf7
JON REY L. LUMAYAG <jaezer.razor@gmail.com>
2024-06-20 06:34:03 +0800

    Change BrowserRouter to HashRouter

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 // const localhost = "10.0.2.2";
 // const network_ip = "127.0.0.1";
-// const dev_url = `http://${network_ip}:3001/api/v1/`;
-const live_url = "https://voter-info.onrender.com/api/v1/";
+// const url = `http://127.0.0.1:3001/api/v1/`;
+const url = "https://voter-info.onrender.com/api/v1/";
 
 export default axios.create({
-  baseURL: live_url,
+  baseURL: url,
   timeout: 5000,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: live_url,
+  baseURL: url,
   timeout: 5000,
   headers: { "Content-Type": "application/json" },
   // withCredentials: true,
